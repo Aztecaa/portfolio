3ad20b3c43200c5dd6fc2452a5b3c7a3a5c2339d
Aztecaa <Aztecaned@gmail.com>
2023-03-26 04:06:18 -0300

    home correction

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,17 +16,13 @@ const Home = () => {
         ></video>
       </div>
       <div className="home_content">
-        <div className="myPhoto">
-          <img className=" levitate" src={me} alt="" />
-        </div>
-        <div>
-          <div className="home_welcome">
-            <h1>
-              Hola! Soy <span>Ignacio G. Escudero</span>
-            </h1>
-            <hr className="home_hr" />
-            <h2 className="home_text">Frontend development</h2>
-          </div>
+        <img className="my-photo levitate" src={me} alt="" />
+        <div className="home_welcome">
+          <h1>
+            Hola! Soy <span>Ignacio G. Escudero</span>
+          </h1>
+          <hr className="home_hr" />
+          <h2 className="home_text">Frontend development</h2>
         </div>
       </div>
     </section>
